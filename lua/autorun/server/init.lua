3b7ffa06790567693f20343f9f6fed41df7bff13
RouteSixtySix <amlaldu93@gmail.com>
2021-10-17 10:18:30 +0200

    Update init.lua
    fix typo

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -9,7 +9,7 @@
 LibC = LibC or {}
 
 include("autorun/server/sv_core.lua")
-include("autorun/server/svl_sql.lua")
+include("autorun/server/sv_sql.lua")
 AddCSLuaFile("autorun/client/cl_init.lua")
 
 LibC:Log("LibC: Loaded.")
