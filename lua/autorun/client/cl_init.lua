46d9559281f286fa9d0e29103eb80ed3ba1e0085
RouteSixtySix <amlaldu93@gmail.com>
2021-10-19 06:27:16 +0200

    Update cl_init.lua
    Edit copyright notice

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -1,4 +1,4 @@
 concommand.Add("libc_version", function()
 	print("LibClassiC version 1.2.1")
-	print("Copyright Amlal El Mahrouss & ClassiC all rights reserved")
+	print("Copyright Amlal \"ALTernative\" El Mahrouss & ClassiC all rights reserved")
 end)
