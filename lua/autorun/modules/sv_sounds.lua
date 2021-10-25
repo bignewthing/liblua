cd5f657caf4d203554b36b41363510ec8249ded7
RouteSixtySix <64403535+amllx@users.noreply.github.com>
2021-10-25 09:45:38 +0200

    edit sounds

--- lua/autorun/modules/sv_sounds.lua
+++ lua/autorun/modules/sv_sounds.lua
@@ -3,20 +3,20 @@ sound.Add({
 	name = 			"COIN.PICKUP",
 	channel = 		CHAN_ITEM,
 	volume = 		1,
-	sound = 		Sound("pickup.mp3")
+	sound = 		Sound("res/pickup.mp3")
 })
 
 sound.Add({
 	name = 			"COIN.DEPOSIT",
 	channel = 		CHAN_ITEM,
 	volume = 		1,
-	sound = 		Sound("items/medshot4.wav")
+	sound = 		Sound("ambient/tones/elev1.wav")
 })
 
 sound.Add({
 	name = 			"COIN.WITHDRAW",
 	channel = 		CHAN_ITEM,
 	volume = 		1,
-	sound = 		Sound("items/ammo_pickup.wav")
+	sound = 		Sound("ambient/tones/equip4.wav")
 })
 
--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -12,9 +12,3 @@ include("autorun/server/sv_core.lua")
 include("autorun/server/sv_config.lua")
 include("autorun/server/sv_moduleLoad.lua")
 AddCSLuaFile("autorun/client/cl_init.lua")
-
-LibC:Log("--------------------------------");
-LibC:Log("--------------------------------");
-LibC:Log("LibC: has been Loaded.");
-LibC:Log("--------------------------------");
-LibC:Log("--------------------------------");
